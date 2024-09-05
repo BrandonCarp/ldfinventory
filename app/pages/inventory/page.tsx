676b63fd3444ae21793ad4fe3984d4dd0a278ad6
Brandon C <brancarp93@gmail.com>
2024-09-05 13:46:41 -0400

    table updates

--- app/pages/inventory/page.tsx
+++ app/pages/inventory/page.tsx
@@ -7,8 +7,69 @@ import {
   TableCell,
 } from "@nextui-org/table";
 
+const rows = [
+  {
+    key: "1",
+    name: "",
+    role: "",
+    status: "",
+  },
+  {
+    key: "1",
+    name: "",
+    role: "",
+    status: "",
+  },
+  {
+    key: "1",
+    name: "",
+    role: "",
+    status: "",
+  },
+  {
+    key: "1",
+    name: "",
+    role: "",
+    status: "",
+  },
+];
+
+const columns = [
+  {
+    key: "name",
+    label: "Part Name",
+  },
+  {
+    key: "description",
+    label: "Part Description",
+  },
+  {
+    key: "quantity",
+    label: "Quantity",
+  },
+];
+
+const getKeyValue = (item: any, columnKey: number) => {
+  // Example logic: Return the value from the item object based on columnKey
+  return item[columnKey] || "Default Value";
+};
 const LiveInventory = () => {
-  return <div>Live Inventory Page</div>;
+  return (
+    <Table aria-label="LDF Live Table">
+      <TableHeader columns={columns}>
+        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+      </TableHeader>
+      <TableBody items={rows}>
+        {(item) => (
+          <TableRow key={item.key}>
+            {(columnKey: any) => (
+              <TableCell>{getKeyValue(item, columnKey)}</TableCell>
+            )}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
 };
 
 export default LiveInventory;
