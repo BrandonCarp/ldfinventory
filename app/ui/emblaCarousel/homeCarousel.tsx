22763e9e1f30ea66f407fe24f980dd3dfc26e0a6
Brandon Carpenter <brandoncarpenter@lynmarbuilders.com>
2024-09-12 11:28:10 -0400

    image update

--- app/ui/emblaCarousel/homeCarousel.tsx
+++ app/ui/emblaCarousel/homeCarousel.tsx
@@ -5,22 +5,28 @@ import Autoplay from "embla-carousel-autoplay";
 import ldf1 from "../images/1.jpeg";
 import ldf2 from "../images/2.jpeg";
 import ldf3 from "../images/3.jpg";
+import ldf4 from "../images/4.jpg";
+import ldf5 from "../images/5.jpg";
+
 
 export default function HomeCarousel() {
   const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()]);
-  const images = [ldf1, ldf2, ldf3];
+  const images = [ldf1, ldf2, ldf3, ldf4, ldf5];
   return (
     <div className=" overflow-hidden bg-white  " ref={emblaRef}>
       <div className="flex">
         {images.map((image, index) => (
-          <Image
-            key={index}
-            className="flex-[0_0_100%] min-w-0"
-            src={image}
-            alt={`Slide ${index + 1}`}
-            width={150}
-            // height={250} // Adjust this to your desired height
-          />
+         <Image
+         key={index}
+         className="flex-[0_0_100%] min-w-0 "
+         src={image}
+         alt={`Slide ${index + 1}`}
+     
+         sizes="(max-width: 600px) 100vw, 50vw"
+         width={100}
+         height={200}
+        
+       />
         ))}
       </div>
     </div>
